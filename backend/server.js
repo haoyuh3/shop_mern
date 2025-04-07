6f4f223f4bff1688860956b505f34099c58804e2
haoyuh3 <1187296801@qq.com>
2025-04-07 20:49:26 +0800

    day5

--- backend/server.js
+++ backend/server.js
@@ -10,23 +10,36 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-
+/*
+加载环境变量 .env
+	•	连接 MongoDB（通过 Mongoose）
+	•	设置解析 JSON 的中间件
+	•	加载路由 /api/products
+	•	启动服务监听端口
+ */
 dotenv.config()
 
 connectDB()
 
 const app = express()
-
+//process node.js 的全局变量
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
-}
+} // 记录日志细节
 
 app.use(express.json())
-
+/*
+将不同模块的 API 路由挂载到主路由上：
+	•	/api/products → 商品相关逻辑
+	•	/api/users → 用户登录、注册
+	•	/api/orders → 订单逻辑
+	•	/api/upload → 图片或文件上传
+ */
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
+
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
@@ -43,7 +56,7 @@ if (process.env.NODE_ENV === 'production') {
   )
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running....')
+    res.json({ status: 'ok', message: 'API is running....' })
   })
 }
 
